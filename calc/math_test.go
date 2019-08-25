464b953457a0574cd546f0e9e32530737b882fba
stevedesilva <7001268+stevedesilva@users.noreply.github.com>
2019-08-25 16:21:44 +0100

    refactor

--- calc/math_test.go
+++ calc/math_test.go
@@ -2,10 +2,21 @@ package calc
 
 import "testing"
 
-func TestMath(t *testing.T) {
+func TestAdd(t *testing.T) {
 	result := Add(1, 1)
 	if result != 2 {
 		t.Errorf("Expecting result %v, got %v", 2, result)
 	}
 
 }
+
+// func TestAddVaradic (t *testing.T) {
+// 	result := struct[] {
+// 		nums ..int
+// 		res
+// 	}
+// 	if result != 2 {
+// 		t.Errorf("Expecting result %v, got %v", 2, result)
+// 	}
+
+// }
