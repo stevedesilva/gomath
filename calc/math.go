f8a2c1c6af75bf71799338a56af9fd95192b15f3
stevedesilva <7001268+stevedesilva@users.noreply.github.com>
2019-08-25 16:49:48 +0100

    release 2.0.0

--- calc/math.go
+++ calc/math.go
@@ -1,16 +1,36 @@
 package calc
 
+import "errors"
+
 // Add sums two values returning result
+//
+// v1:
 // func Add(a int, b int) int {
 // 	res := a + b
 // 	return res
 // }
-func Add(numbers ...int) int {
-	sum :=0
+//
+// v2
+// func Add(numbers ...int) int {
+// 	sum :=0
+//
+// 	for _, num := range numbers {
+// 		sum += num
+// 	}
+// 	return sum
+// }
+//
+func Add(numbers ...int) (int, error) {
+	sum := 0
+
+	if len(numbers) > 2 {
+		return sum, errors.New("Provide more that 2 numbers")
+	}
 
 	for _, num := range numbers {
 		sum += num
 	}
 
-	return sum
+	return sum, nil
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/stevedesilva/gomath
+module github.com/stevedesilva/gomath/v2
 
 go 1.12
