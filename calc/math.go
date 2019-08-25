c06a9f3db1108b73f612ea11d2e5586dc9b22af3
stevedesilva <7001268+stevedesilva@users.noreply.github.com>
2019-08-25 16:12:24 +0100

    variadic

--- calc/math.go
+++ calc/math.go
@@ -1,6 +1,16 @@
 package calc
+
 // Add sums two values returning result
-func Add(a int, b int) int {
-	res := a + b
-	return res
+// func Add(a int, b int) int {
+// 	res := a + b
+// 	return res
+// }
+func Add(numbers ...int) int {
+	sum :=0
+
+	for _, num := range numbers {
+		sum += num
+	}
+
+	return sum
 }
